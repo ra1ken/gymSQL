042461083737f2fb1e1f4624a29f491b170b92ed
Luky173 <lukas.karas.2019@gmail.com>
2026-02-12 22:38:23 +0100

    YAY MY WRITING INTO DB WORKS ITS ONLY SMOOTH SAILIING FROM NOW!!!!!!!!!!!

--- gym.py
+++ gym.py
@@ -1,1 +1,20 @@
 import os
+import sqlite3
+
+
+
+
+if not (os.path.exists("gym.db")):
+    con = sqlite3.connect("gym.db")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE gymstats(exercise, weight, date, reps)")
+    cur.execute("INSERT INTO gymstats VALUES('bench','100','12.2.2026',8)")
+    con.commit()
+    con.close()
+
+
+
+# else:
+   # print("db exists :)")
+#table_list = [a for a in cur.execute("SELECT name FROM sqlite_master WHERE type = 'table'")]
+#print(table_list)
